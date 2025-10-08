d4e80e68d7de26bbd638cbd309e00cd48294fa0b
SirKneel <neilorion3@gmail.com>
2025-10-09 02:29:31 +0800

    Update server.js

--- backend/server/server.js
+++ backend/server/server.js
@@ -277,7 +277,7 @@ app.use(cors({
   origin: "*",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept", "Origin"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept", "Origin", "X-Requested-With"],
   exposedHeaders: ["Content-Length", "X-Foo", "X-Bar"],
   preflightContinue: false,
   optionsSuccessStatus: 200
